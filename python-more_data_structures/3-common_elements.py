cd62d6245cece2463d98ff7807fdc3b44a8c96ea
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-16 13:50:39 +0000

    Task 3: common_elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that returns a set of common elements in two sets
+"""
+
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of elements present in both sets
+    """
+    return set_1 & set_2
