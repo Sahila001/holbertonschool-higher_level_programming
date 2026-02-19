5898ef0accee6e6ff873a4da083c35a7b7f0074c
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-19 13:21:18 +0000

    Add README for python-test_driven_development

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that adds two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    a and b must be integers or floats.
+    If float, they are casted to integers.
+
+    Returns the addition of a and b.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
