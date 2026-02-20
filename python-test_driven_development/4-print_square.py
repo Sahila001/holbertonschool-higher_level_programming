61d0d75c5c501364dd0703ca5f541693d7eb11fc
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-20 16:16:52 +0000

    Add print_square function with type and value checks

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""
+Module that prints a square with the # character.
+"""
+
 
 def print_square(size):
-    """Prints a square with the character #"""
+    """Prints a square of size `size` using #"""
+
+    # Check type
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
+    # Check value
     if size < 0:
-        raise ValueError("
+        raise ValueError("size must be >= 0")
+
+    # Print square
+    for _ in range(size):
+        print("#" * size)
