8040fb172ff3cf9b4840f1ff4528089313037bb3
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-21 13:33:11 +0000

    Fix Task 4: added missing argument test case

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-Module that prints a square with the # character.
-"""
+"""Module for print_square function."""
 
 
 def print_square(size):
-    """Prints a square of size `size` using #"""
-
-    # Check type
+    """Prints a square with the character #."""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
-    # Check value
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    # Print square
     for _ in range(size):
         print("#" * size)
