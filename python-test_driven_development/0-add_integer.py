3199066d091595ba4cb76d7419d1ed65a26c91cc
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-21 13:15:37 +0000

    Fix task 0 with inf, nan and missing args

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,28 +1,11 @@
 #!/usr/bin/python3
-"""
-Module for adding two integers.
-"""
+"""Module for adding two integers."""
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-    """
-
+    """Adds two integers or floats after casting to int."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    try:
-        a = int(a)
-    except (ValueError, OverflowError):
-        raise TypeError("a must be an integer")
-
-    try:
-        b = int(b)
-    except (ValueError, OverflowError):
-        raise TypeError("b must be an integer")
-
-    return a + b
+    return int(a) + int(b)
