02f39f0eda6cd8e77a5d7d6009880cca084c7415
Sahila001 <sahilamikayilova001@gmail.com>
2026-02-28 13:55:15 +0000

    Create Square class with size validation and custom __str__

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 11-square
+Defines a Square class inheriting from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle with custom __str__"""
+
+    def __init__(self, size):
+        """Initialize a Square with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return string representation [Square] width/height"""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
