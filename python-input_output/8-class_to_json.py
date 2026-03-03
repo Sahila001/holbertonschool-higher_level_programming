341db8e03435289720ec02c4fe5e243f48ec9e5a
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-03 17:12:29 +0000

    Fix PEP8 for class_to_json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""
-Function that returns the dictionary description of an object for JSON serialization
+"""Function that returns a dictionary description of an object
+for JSON serialization.
 """
 
+
 def class_to_json(obj):
-    """Returns a dictionary with all simple attributes of an object"""
+    """Returns a dictionary with all simple attributes of an object."""
     return obj.__dict__.copy()
