95a0a161787977133ea7e57497e64ab627b8b21b
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-03 16:39:32 +0000

    Add write_file function using with statement

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Module 1-write_file
+Defines a function that writes a string to a UTF-8 text file
+and returns the number of characters written.
+"""
+
+
 def write_file(filename="", text=""):
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return f.write(text)
+    """Write text to a UTF-8 file and return number of characters"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
