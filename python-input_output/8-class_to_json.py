a705250f349c40beb659572fca6b878cd4632fba
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-03 17:04:03 +0000

    Add class_to_json function for JSON serialization of objects

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description of an object for JSON serialization
+"""
+
+def class_to_json(obj):
+    """Returns a dictionary with all simple attributes of an object"""
+    return obj.__dict__.copy()
