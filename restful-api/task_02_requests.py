76a1b83613140f27ec50ebffa33ecbbddda31cb7
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-07 18:41:41 +0000

    Fetch posts from API and save them to CSV

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post.get("title"))
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        data = [
+            {
+                "id": post.get("id"),
+                "title": post.get("title"),
+                "body": post.get("body")
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv", "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+
+            writer.writeheader()
+            writer.writerows(data)
