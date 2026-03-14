cd642ddca8ee1432ed27919122008ccc9831a54f
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-14 13:18:29 +0000

    Fix SQL queries and formatting for states filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument provided by the user.
+Fix: SQL query-də dırnaq və sıralama dəqiqliyi
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma (4 arqument götürürük)
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    # Bağlantı zamanı arqumentlərin sırasına və portun integer olmasına bax
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     
     cur = db.cursor()
-    
-    # .format() istifadə edərək sorğunu qururuq
-    # BINARY istifadə edirik ki, həm böyük/kiçik hərf, həm də tam uyğunluq yoxlanılsın
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
-    
-    cur.execute(query)
+    # Ştat adını '{}' dırnaqları daxilinə alırıq
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(sys.argv[4]))
     
     rows = cur.fetchall()
     for row in rows:
         print(row)
     
-    # Bağlantıları təmizləyirik
     cur.close()
     db.close()
