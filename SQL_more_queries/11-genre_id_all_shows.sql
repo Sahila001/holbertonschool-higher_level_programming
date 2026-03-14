9435965df17ade9fb74f4f487655bc3ce5e3f502
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-14 12:36:44 +0000

    Lists all shows including those without a genre

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,7 @@
--- 11-genre_id_all_shows.sql
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without a genre linked.
+-- Results are sorted by show title and then by genre_id.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
