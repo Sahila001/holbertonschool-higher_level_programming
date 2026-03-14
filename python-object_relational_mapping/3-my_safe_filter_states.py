2339fad77cbb880486e2df115f74213a45898008
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-14 13:26:03 +0000

    Safe from SQL

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 """
-Lists all values in the states table where name matches the argument.
-This script is safe from MySQL injections.
+Safe MySQL query
+Takes 4 arguments: username, password, database name, state name
+Displays all states where name matches, safe from SQL injection
 """
-import MySQLdb
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    
-    cur = db.cursor()
-    
-    # SQL Injection-dan qorunmaq üçün %s yer tutucusundan istifadə edirik
-    # Diqqət: Sorğu daxilində %s dırnağa ALINMIR. 
-    # Məlumat isə ikinci arqument kimi (tuple formatında) verilir.
+    if len(sys.argv) != 5:
+        print("Usage: ./3-my_safe_filter_states.py <user> <password> <database> <state_name>")
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    # Safe query using parameterized statement
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (sys.argv[4],))
-    
-    rows = cur.fetchall()
-    for row in rows:
+    cursor.execute(query, (state_name,))
+
+    # Fetch and print results
+    for row in cursor.fetchall():
         print(row)
-    
-    # Bağlantıları təmizləyirik
-    cur.close()
+
+    cursor.close()
     db.close()
