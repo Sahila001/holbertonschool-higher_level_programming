7a5daf5a67fc0595422d1b1820c234aab1f5c22b
Sahila001 <sahilamikayilova001@gmail.com>
2026-03-15 17:36:43 +0000

    Safe filter states from SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""
-Displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument, safe from MySQL injection.
-"""
+"""Safe script that lists states matching the argument"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
-    
-    cur = db.cursor()
-    
-    # SQL Injection-dan qorunmaq üçün:
-    # 1. %s-i dırnaq (') daxilinə ALMA.
-    # 2. Arqumenti execute-un ikinci parametri olaraq (tuple kimi) ötür.
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-    
-    rows = cur.fetchall()
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    
-    # Bağlantıları bağlayırıq
-    cur.close()
+
+    cursor.close()
     db.close()
