60f2e786fdb51ba719b307890c2fc6d7df43a2e7
Sahila001 <sahilamikayilova001@gmail.com>
2026-06-05 21:40:45 +0400

    JavaScript DOM manipulation tasks 0-8 completed

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#add_item').addEventListener('click', function () {
+        const li = document.createElement('li');
+        li.textContent = 'Item';
+        document.querySelector('.my_list').appendChild(li);
+    });
+});
---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+        .then(response => response.json())
+        .then(data => {
+            const list = document.querySelector('#list_movies');
+
+            data.results.forEach(movie => {
+                const li = document.createElement('li');
+                li.textContent = movie.title;
+                list.appendChild(li);
+            });
+        });
+});
